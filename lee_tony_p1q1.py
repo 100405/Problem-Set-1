893c140dee093ce6b58146fcf5ab14a4eb2fde5c
tony lee <kaspurly@gmail.com>
2021-09-27 12:36:50 -0700

    Added description

--- lee_tony_p1q1.py
+++ lee_tony_p1q1.py
@@ -1,1 +1,30 @@
 # Quiz Creation Activity
+
+# The user will talk a quiz
+#
+
+print("What is 2 + 5")
+ans = float(input())
+if ans == 7:
+    print("Correct!")
+    score = 0 + 1
+else:
+    print("Incorrect ;=;")
+    score = score
+print("What is 8 + 5")
+ans = float(input())
+if ans == 13:
+    print("Correct!")
+    score += score + 1
+else:
+    print("Incorrect ;=;")
+    score = score
+print("What is 98 + 7")
+ans = float(input())
+if ans == 105:
+    print("Correct!")
+    score += score + 1
+else:
+    print("Incorrect ;=;")
+    score = score
+print(score)
