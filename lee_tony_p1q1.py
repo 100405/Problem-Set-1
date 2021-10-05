2f129788c865a9bddcd4698c6c7aa2d1230ec95d
tony lee <kaspurly@gmail.com>
2021-10-05 09:50:22 -0700

    Added math question

--- lee_tony_p1q1.py
+++ lee_tony_p1q1.py
@@ -2,30 +2,38 @@
 
 # The user will talk a quiz
 #
+import time
+from rich import print
+score_now = 0
 
 print("What is 2 + 5")
 ans = float(input())
 if ans == 7:
+    score_now = score_now + 1
     print("Correct!")
-    score = 0 + 1
+    print(f"You got {score_now}/1 questions correct so far")
+    score_now = score_now + 1
 else:
     print("Incorrect ;=;")
-    score = score
-print("What is 8 + 5")
-ans = float(input())
-if ans == 13:
+    print("You got {score_now}/1 questions correct so far")
+    score_now = score_now
+print("What is the colour of the colour red?")
+ans = input()
+if ans == "red":
+    score_now = score_now + 1
     print("Correct!")
-    score += score + 1
+    print(f"You got {score_now}/2 questions correct so far")
 else:
     print("Incorrect ;=;")
-    score = score
+    print(f"You got {score_now}/2 questions correct so far")
+    score_now = score_now
 
 # Math Question
 print("What is 98 + 7")
 ans = float(input())
 if ans == 105:
     print("Correct!")
-    score += score + 1
+
 else:
     print("Incorrect ;=;")
     score = score
